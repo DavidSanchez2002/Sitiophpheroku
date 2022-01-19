1572525ea161bdbe7db77b1119cedbc396bb1baa
DavidSanchez2002 <82877214+DavidSanchez2002@users.noreply.github.com>
2022-01-19 10:11:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejemplo</title>
+    </head>
+    <body>
+
+        <?php
+            echo "<h2>¡Hola, soy un script de PHP!</h2>";
+        ?>
+
+    </body>
+</html>
